1047854f6d4a5b86fbe0e4035625df33d52641e0
Angad <80452785+Angad-Godara@users.noreply.github.com>
2023-12-06 18:27:17 +0530

    minor test fix

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Home from './components/Content/Home';
 import Navbar from './components/Navbar/Navbar';
 import Sidebar from './components/Sidebar/Sidebar';
 
 function App() {
+
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    // Update the width when the window is resized
+    const handleResize = () => {
+      setWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    // Clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
-    <div>
-      <Sidebar />
-      <Navbar />
-      <Home />
-    </div>
+    <>
+      {
+        width > 800
+          ?
+          <div>
+            <Sidebar />
+            <Navbar />
+            <Home />
+          </div>
+          :
+          <div data-gs
+            data-productidentifier="10"
+            data-apikey="GS_f5d91c51-48eb-4d3c-8654-0a61a03e4a58"
+            data-layout="small"
+            data-lang="en">
+          </div>
+      }
+    </>
   );
 }
 
